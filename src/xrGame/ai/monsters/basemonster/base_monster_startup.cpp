2c12caf527d0fd2b9b36913eb93e6a858b5ad196
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-06-22 12:37:53 +0300

    схлопнуть

--- src/xrGame/ai/monsters/basemonster/base_monster_startup.cpp
+++ src/xrGame/ai/monsters/basemonster/base_monster_startup.cpp
@@ -146,12 +146,7 @@ void CBaseMonster::Load(LPCSTR section)
 	{
 		LPCSTR protections_sect = pSettings->r_string(section, "protections_sect");
 		m_fSkinArmor = READ_IF_EXISTS(pSettings,r_float,protections_sect,"skin_armor", 0.f);
-		float defaultHitFraction = 0.1f;
-		if (EngineExternal().ClearSkyMode())
-		{
-			defaultHitFraction = READ_IF_EXISTS(pSettings, r_float, protections_sect, "hit_fraction", defaultHitFraction);
-		}
-		m_fHitFracMonster = READ_IF_EXISTS(pSettings, r_float, protections_sect, "hit_fraction_monster", defaultHitFraction);
+		m_fHitFracMonster = READ_IF_EXISTS(pSettings, r_float, protections_sect, "hit_fraction_monster", 0.1f);
 	}
 
 	m_force_anti_aim						=	false;
