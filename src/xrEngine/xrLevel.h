036a6601e8c85d8b05ab9e85683b086611622313
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-06-22 12:37:51 +0300

    Add some features for future

--- src/xrEngine/xrLevel.h
+++ src/xrEngine/xrLevel.h
@@ -446,4 +446,6 @@ constexpr u32 MAX_NODE_XZ = NodePosition::MAX_XZ;
 
 constexpr u32 XRAI_MINIMAL_VERSION = 10;
 constexpr u32 XRAI_CURRENT_VERSION = 11;
+constexpr u32 XRAI_SOC_CURRENT_VERSION = 8;
+
 #pragma warning(pop)
