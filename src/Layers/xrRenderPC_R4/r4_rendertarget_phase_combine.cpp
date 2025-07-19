81a961fb2b8011e3d2928e8720dcae882cc696c9
LVutner <kkamil724@gmail.com>
2025-07-19 20:18:31 +0300

    initial nvg shader, enabled by default

--- src/Layers/xrRenderPC_R4/r4_rendertarget_phase_combine.cpp
+++ src/Layers/xrRenderPC_R4/r4_rendertarget_phase_combine.cpp
@@ -386,6 +386,13 @@ void CRenderTarget::phase_combine()
 		GPU_EVENT(PhaseAberration);
 		PhaseAberration();
 	}
+	
+	//Aight, so here we should enable/disable nvg
+	{
+		PIX_EVENT(phase_nvg);
+		phase_nvg();
+	}
+	
 	{
 		GPU_EVENT(phase_pp);
 		phase_pp();
