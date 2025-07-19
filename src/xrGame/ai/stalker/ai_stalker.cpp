e73df946e3118b63d38842defa020f8fb5f77c58
v2v3v4 <terran.erre@mail.ru>
2025-07-19 20:13:13 +0300

    regular update

--- src/xrGame/ai/stalker/ai_stalker.cpp
+++ src/xrGame/ai/stalker/ai_stalker.cpp
@@ -548,7 +548,7 @@ void CAI_Stalker::LookAtActor(CBoneInstance* headBone) {
 	if (!g_Alive())
 		return;
 
-	if (!Actor())
+	if (!g_actor)
 		return;
 
 	if (wounded())
@@ -991,6 +991,7 @@ void CAI_Stalker::shedule_Update		( u32 DT )
 		memory().update					(dt);
 		STOP_PROFILE
 
+		update_can_kill_info();
 		STOP_PROFILE
 	}
 
