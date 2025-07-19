9bfdfc13f35c3a906fe3857e6d4253dbe1a507da
Shtrecker <gogels235@gmail.com>
2025-07-19 20:18:31 +0300

    Привязка шейдера к классу ПНВ

--- src/Layers/xrRenderPC_R4/r4_rendertarget_phase_combine.cpp
+++ src/Layers/xrRenderPC_R4/r4_rendertarget_phase_combine.cpp
@@ -4,6 +4,8 @@
 
 #include "../xrRender/dxEnvironmentRender.h"
 
+ENGINE_API extern bool turn_nvg; //Переместить в более подходящее место
+
 void CRenderTarget::DoAsyncScreenshot()
 {
 	//	Igor: screenshot will not have postprocess applied.
@@ -388,6 +390,7 @@ void CRenderTarget::phase_combine()
 	}
 	
 	//Aight, so here we should enable/disable nvg
+	if (turn_nvg)
 	{
 		PIX_EVENT(phase_nvg);
 		phase_nvg();
